bb6b5521c7cd5018fa1faa89abd4cc4b90bdaa07
salina bade <bsalina101@gmail.com>
2025-05-16 17:03:32 +1000

    Initial commit - FastAPI calculator project

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+class Operation(BaseModel):
+    a: float
+    b: float
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/add")
+def add(op: Operation):
+    return {"result": op.a + op.b}
+
+@app.post("/subtract")
+def subtract(op: Operation):
+    return {"result": op.a - op.b}
+
+@app.post("/multiply")
+def multiply(op: Operation):
+    return {"result": op.a * op.b}
+
+@app.post("/divide")
+def divide(op: Operation):
+    if op.b == 0:
+        raise HTTPException(status_code=400, detail="Cannot divide by zero")
+    return {"result": op.a / op.b}
+
+
+
+
